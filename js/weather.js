bda790fa134189b91a55386036726bf31cceeb7c
Diogo Rosa <diogopt.rosa@gmail.com>
2016-05-10 18:37:43 +0100

    Version 1 finished.

--- js/weather.js
+++ js/weather.js
@@ -1,89 +1,228 @@
-var app = angular.module('userLocal', []);
+(function() {
+    var app = angular.module('myApp', []);
+    
+    //Welcome/Loading directive before weather is showed
+    app.directive('welcomeDiv', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'welcome-div.html'
+        };
+    });
+    
+    //Directive for currently weather predictions
+    app.directive('currentWeather',['$http', function($http){
+       return{
+           restrict: 'E',
+           templateUrl: 'current-weather.html',
+           controller:function(){
+                var today = this;
+                today.info = [];
+                today.date = new Date();
 
-var forecast = [{
-    date: 1465804800000,//Today Date
-    city: "consenza",
-    country: "IT",
-    main: "sun",
-    temp: 23,
-    temp_min: 18,
-    temp_max: 25,
-    humidity: 67,
-    wind_speed: 5,
-    sunrise: new Date(1465796700000),
-    sunset: new Date(1465849800000)
-    },
-                
-    {
-    date: 1465891200000,
-    city: "consenza",
-    country: "IT",
-    main: "rain",
-    temp: 20,
-    temp_min: 16,
-    temp_max: 25,
-    humidity: 60,
-    wind_speed: 3,
-    sunrise: new Date(1465882200000),
-    sunset: new Date(1465936643000)
-    } ,
-                
-    {
-    date: 1465977600000,
-    city: "consenza",
-    country: "IT",
-    main: "sun",
-    temp: 22,
-    temp_min: 20,
-    temp_max: 28,
-    humidity: 75,
-    wind_speed: 6,
-    sunrise: new Date(1465968841000),
-    sunset: new Date(1466023392000)
-    } ,
-                
-    {
-    date: 1466064000000,
-    city: "consenza",
-    country: "IT",
-    main: "sun",
-    temp: 25,
-    temp_min: 21,
-    temp_max: 26,
-    humidity: 72,
-    wind_speed: 5,
-    sunrise: new Date(1466054684000),
-    sunset: new Date(1466109894000)
-    } 
-];
+                today.setInfo = function (lat, lng) {            
+                    if(lat > 0) {
+                        if(today.info.length == 0){
+                            $http.get('http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lng + '&units=metric&appid=2064cd1f6b177f54627e549645285203')
+                                 .then(function(resp) {
+                                    today.info = resp.data;
 
-app.controller("GetLocation", function($scope){
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position){
-            var lat = position.coords.latitude;
-            var lng = position.coords.longitude;
-            $scope.$apply(function(){
-                $scope.lat = lat;
-                $scope.lng = lng;
-            });
-        });
-    } else {
-        $scope.error = "Geolocation is not supported by this browser.";
-    }
-});
+                                    if(today.info.sys.sunset*1000 < today.date)
+                                        today.dayNight = "night";
+                                    else
+                                        today.dayNight = "day";                            
+                                 }, function(error) {
+                                    today.error = error;
+                                 });
+                        }
+                    }
 
-app.controller("GetForecast", function(){
-    this.today = forecast[0];
+                    return today.info.length != 0;
+                };
+           },
+           controllerAs: 'current'
+       };
+    }]);
     
-    this.pred = {};
+    //Three days forecast counting after the current date
+    app.directive('threeDayWeather',['$http', function($http){
+        return{
+            restrict: 'E',
+            templateUrl: 'three-day-weather.html',
+            controller: function(){
+                var forecast = this;
+                forecast.info = [];
+
+                forecast.setInfo = function(lat, lng) {  
+                    if(lat > 0) {
+                        if(forecast.info.length == 0){
+                            $http.get('http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lng + '&units=metric&appid=2064cd1f6b177f54627e549645285203')
+                                 .then(function(resp) {
+                                    var today = new Date();
+                                    today.setHours(0,0,0,0);
+
+                                    forecast.info = resp.data;
+
+                                    forecast.weatherInfo = [];
+
+                                    if(forecast.info.list.length > 0){
+                                        var i;
+                                        var start = 0;
+                                        var end = 0;
+                                        for (i = 0; i < forecast.info.list.length; i++){
+                                            var currentDate = new Date(forecast.info.list[i].dt*1000);
+                                            currentDate.setHours(0,0,0,0);
+
+                                            //number of days after current date
+                                            var days = ((currentDate.getTime() - today.getTime()) / (1000*60*60*24));
+
+                                            if(days == 1 && start == 0){
+                                                start = i;
+                                            }
+
+                                            if(days == 4){
+                                                end = i;
+                                                break;
+                                            }
+                                        }
+                                        
+                                        var x = 0;
+                                        var min = forecast.info.list[0].main.temp_min;
+                                        var max = 0;
+                                        var t = 0;
+
+                                        //count number of daily forecast.info.list[index].main.temp
+                                        var count = 0; 
+                                        var hours = [];
+                                        var keywords = [];
+
+                                        for(i = start; i < end; i++){
+                                            //console.log(forecast.info.list[i]);
+                                            var date1 = 0;
+                                            var date2 = 0;
+
+                                            hours[count] = forecast.info.list[i];
+
+                                            if(keywords.length == 0){
+                                                keywords[count] = {
+                                                    val: forecast.info.list[i].weather[0].main,
+                                                    count: 1
+                                                };
+                                            }
+                                            else {
+                                                var n;
+
+                                                for(n = 0; n < keywords.length; n++){
+                                                    if(forecast.info.list[i].weather[0].main == keywords[n].val)
+                                                        keywords[n].count += 1;
+                                                    else
+                                                        keywords[n + 1] = {
+                                                            val: forecast.info.list[i].weather[0].main,
+                                                            count: 1
+                                                        };
+
+                                                    //console.log(keywords[n]);
+                                                }
+                                            }
+
+                                            count = count + 1;
+                                            t = t + forecast.info.list[i].main.temp;
+
+                                            if(forecast.info.list[i].main.temp_max > max)
+                                                max = forecast.info.list[i].main.temp_max;
+
+                                            if(forecast.info.list[i].main.temp_min < min)
+                                                min = forecast.info.list[i].main.temp_min;
+
+                                            if(i < end){
+                                                date1 = new Date(forecast.info.list[i].dt*1000);
+                                                date1.setHours(0,0,0,0);
+                                                date2 = new Date(forecast.info.list[i + 1].dt*1000);
+                                                date2.setHours(0,0,0,0);
+
+                                                var days = ((date2.getTime() - date1.getTime()) / (1000*60*60*24));
+
+                                                if(days == 1){
+                                                    var nKey;
+                                                    var main_max = keywords[0].count;
+                                                    var main;
+
+                                                    for(nKey = 0; nKey < keywords.length; nKey++){
+                                                        main = main_max >= keywords[nKey].count ? keywords[nKey].val : main;
+                                                    }
+
+                                                    t = t / count; 
+
+                                                    forecast.weatherInfo[x] = {
+                                                        date: date1,
+                                                        hours: hours,
+                                                        temp: t,
+                                                        temp_min: min,
+                                                        temp_max: max,
+                                                        main: main
+                                                    };
+
+                                                    x = x + 1;
+
+                                                    t = 0;
+                                                    count = 0;
+                                                    hours = [];
+                                                }
+                                            }  
+
+                                        }
+                                    }
+
+                                    //console.log(forecast.weatherInfo);
+                                 }, function(error) {
+                                    forecast.error = error;
+                                 });
+                        }
+                    }
+
+                    return forecast.info.length != 0;
+                }
+            },
+            controllerAs: 'forecast'
+        };
+    }]);
     
-    this.pred[0] = forecast[1];
-    this.pred[1] = forecast[2];
-    this.pred[2] = forecast[3];
-                
-    /*$http.get("http://api.openweathermap.org/data/2.5/weather?lat=" + 39.82 + "&" + "lon=" + -7.49 + "&appid=2064cd1f6b177f54627e549645285203").success(function(response){
-        this.data = response.data;
-    });*/
+    //Get current location using html5 geolocation
+    app.controller('LocationController',['$scope', function($scope){ 
+        var coords = this;
+        
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition, showError);
+        }
+        else {
+            coords.error = "Geolocation is not supported by this browser.";
+        }
+        
+        function showPosition (position) {
+            coords.pos = position.coords;
+            $scope.$apply();
+        }
+ 
+        function showError (error) {
+            switch (error.code) {
+                case error.PERMISSION_DENIED:
+                    coords.error = "User denied the request for Geolocation."
+                    break;
+                case error.POSITION_UNAVAILABLE:
+                    coords.error = "Location information is unavailable."
+                    break;
+                case error.TIMEOUT:
+                    coords.error = "The request to get user location timed out."
+                    break;
+                case error.UNKNOWN_ERROR:
+                    coords.error = "An unknown error occurred."
+                    break;
+            }
+            
+            $scope.$apply();
+        }               
+    }]);
+
+    //Controller for card fliping animation
+    app.controller('flipCtrl',['$scope', function($scope) {}]);
     
-    
-});
+})();
